68e375c5873d1af555e8dee8ddfe31e06f201858
bortirif <50165996+bortirif@users.noreply.github.com>
2020-11-28 12:59:15 +0300

    Add files via upload

---
+++ test_to_git.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("first number: " + num);
+             
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("second number: "+ num1);
+
+            
+            int s=num+num1;
+            Console.WriteLine("first number + second number = "+s);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
